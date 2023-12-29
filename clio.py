874a45573487a5e0833a4670a705713320c5a690
kosmass2001 <t8200053@aueb.gr>
2023-12-30 01:31:11 +0200

    recommended stories

--- clio.py
+++ clio.py
@@ -18,6 +18,10 @@ def read_files():
             dataframe2 = pd.read_excel('dataframe2.xlsx')
         else:
             dataframe2 = combine_booking_sheets()
+            #add the ticket cost
+            dataframe2 = add_ticket_cost(dataframe2)
+            #get the profit
+            dataframe2 = profit(dataframe2)
             
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -69,6 +73,18 @@ def combine_review_sheets():
 
     #rename the dataframe to dataframe1
     dataframe1 = combined_df
+    
+    #split recommended['Name of Product Reviewed'] to 2 columns seperated by '|'
+    dataframe1[['product_code', 'product_review']] = dataframe1['Name of Product Reviewed'].str.split('|', expand=True)
+    
+    #drop the column Name of Product Reviewed
+    dataframe1.drop(columns=['Name of Product Reviewed'], inplace=True)
+    
+    #drop the rows where product_code column is na
+    dataframe1.dropna(subset=['product_code'], inplace=True)
+    
+    #drop the rows where product_code does not start with "STL,TO,AU,TL"
+    dataframe1 = dataframe1[dataframe1['product_code'].str.startswith(('STL', 'TO', 'AU', 'TL'))]
     
     dataframe1.to_excel('dataframe1.xlsx', index = False)
     
@@ -130,7 +146,7 @@ def manipulate_dataframe2(dataframe2):
 
     return dataframe2
 
-def add_ticket_cost():
+def add_ticket_cost(dataframe2):
     
     # Load the "ticket cost" sheet into a DataFrame
     ticket_cost_df = pd.read_excel('Booking Stats.xlsx', sheet_name='Ticket Cost')
@@ -138,6 +154,9 @@ def add_ticket_cost():
     # Set the product code as the index for easier access
     ticket_cost_df.set_index('Product Code', inplace=True)
         
+    # Drop the rows where net_price column is na
+    dataframe2.dropna(subset=['net_price'], inplace=True)
+    
     # Concatenate the product_code and Language Code
     dataframe2['Full Product Code'] = dataframe2['product_code'] + dataframe2['Language Code']
     
@@ -172,7 +191,7 @@ def add_ticket_cost():
         
     return dataframe2
 
-def profit():
+def profit(dataframe2):
         
         #calculate the profit
         dataframe2['Profit'] = dataframe2['retail_price'] - dataframe2['Ticket Price']
@@ -197,8 +216,12 @@ def create_successful():
 
 def go_together():
     #find which tours go together. to do that we will use the groupby operator on the second dataframe
-    grouped = dataframe2.groupby('seller_name',)['product_code'].agg(list).reset_index()
+    grouped = dataframe2.groupby('seller_name')['product_code'].agg(list).reset_index()
     grouped.columns = ['seller name', 'product code']
+    
+    #in the column 'product code' we want to keep only the unique values for each seller
+    grouped['product code'] = grouped['product code'].apply(lambda x: list(set(x)))
+    
     grouped.to_excel(output_loc + 'grouped.xlsx', index = False)
 
     #now create a dictionary, so that for each seller we have the name of the tours he provides, instead of their codes
@@ -227,26 +250,49 @@ def create_dictionary():
 
     return mapping
 
+def profit_per_tour():
+    # Now group by 'product_code' and calculate the sum and mean of 'Profit'
+    profit = dataframe2.groupby('product_code')['Profit'].agg(['sum', 'mean']).reset_index()
+
+    # Rename the columns for clarity
+    profit.columns = ['product_code', 'Total Profit', 'Average Profit']
+
+    # Save the grouped data to a new Excel file
+    profit.to_excel(output_loc + 'grouped_profit.xlsx', index=False)
+
 def recommended_stories():
-    #We take the name of the tours of the successful visits
-    recommended = successful.copy()
+    #We take the name of the tours of the visits that were rated 5 stars
+    recommended = dataframe1[dataframe1['Overall Experience'].isin(['Excellent(5 stars)', 'Excellent (5*)', '5*'])]
+        
+    #Keep only the rows that have unique product codes
+    recommended = recommended.drop_duplicates(subset=['product_code'])
     
-    #From the column 'Name of Product Reviewed' we seperate with the '|' and take the second part
-    recommended['Tour_Name'] = recommended['Name of Product Reviewed'].str.split('|').str[1]
+    recommended = recommended[['product_code']]
     
-    recommended[['Tour_Name']].to_excel(output_loc + 'recommended.xlsx', index=False)
+    recommended.to_excel(output_loc + 'recommended_by_stars.xlsx', index = False)
+    
+    #Load the grouped_profit.xslx file
+    grouped_profit = pd.read_excel(output_loc + 'grouped_profit.xlsx')
+    
+    #sort the grouped_profit by the Total Profit column
+    grouped_profit = grouped_profit.sort_values(by=['Total Profit'], ascending=False)
+    
+    #keep the first 10 rows
+    grouped_profit = grouped_profit.head(10)
+    
+    #save the grouped_profit to an excel file
+    grouped_profit.to_excel(output_loc + 'recommended_by_profit.xlsx', index=False)
+        
 
 #START
+#make folder called outputfiles
+if not os.path.exists('outputfiles'):
+    os.makedirs('outputfiles')
+
 #from here and on our program starts
 output_loc = './outputfiles/'
 
 dataframe1, dataframe2 = read_files()
-
-#add the ticket cost
-dataframe2 = add_ticket_cost()
-
-#get the profit
-dataframe2 = profit()
 
 #1. What does a successful tour look like?
 successful = create_successful()
@@ -260,8 +306,11 @@ print('dataframe1 size is ', dataframe1.size)
 print('successful is' , successful.size)
 print('dataframe 2 size is', dataframe2.size)
 
-#which tours go together
+#2. Which tours go together
 go_together()
 
-#which stories would we recommend
+#profit per tour
+profit_per_tour = profit_per_tour()
+
+#3. Which stories would we recommend
 recommended_stories()
